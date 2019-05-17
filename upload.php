e4e7d69f7d89004b4e23db36585048f2ca0d37e9
zhang xiangze <enh6@qq.com>
2019-05-17 16:12:13 +0800

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select file to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload" name="submit">
+</form>
+
+<?php
+$target_file = basename($_FILES["fileToUpload"]["name"]);
+// Check if file already exists
+if ($target_file != NULL) {
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+}
+?>
+
+</body>
+</html>
